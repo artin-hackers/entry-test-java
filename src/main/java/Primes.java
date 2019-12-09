8e6da37e4ad61965c1844bf00594e693bdfd17a4
zdenek-nemec <zd.nemec@gmail.com>
2019-12-09 17:12:06 +0100

    Primes

---
+++ src/main/java/Primes.java
@@ -0,0 +1,21 @@
+public class Primes {
+    public static void main(String[] args) {
+        int counter = 0;
+        for (int number = 2; number <= 21; number++) {
+            boolean divisible = false;
+            for (int divisor = 2; divisor < number; divisor++) {
+                if (number % divisor == 0)
+                {
+                    divisible = true;
+                }
+            }
+            if (!divisible) {
+                counter += 1;
+            }
+            if (counter == 6) {
+                System.out.println(number);
+                break;
+            }
+        }
+    }
+}
