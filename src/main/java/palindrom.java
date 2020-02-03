014d3ded3d1ad9e5bf121794021c22a40b663c3f
BuildTools <unconfigured@null.spigotmc.org>
2020-02-03 17:50:10 +0100

    palindrom

--- src/main/java/palindrom.java
+++ src/main/java/palindrom.java
@@ -1,36 +1,41 @@
 public class palindrom {
     public static void main(String[] args) {
-        //   System.out.println("Hello world");
+       System.out.println("999999");
 
-        for (int i = 1000; i <=9999; i++) {
-            String string;
-            string = Integer.toString(i);
-            if (checkpalindrom(string) == true) {
-                System.out.println(string);
-            }
-        }
+        //for (int i = 100000; i <=999999; i++) {
+          //  String string;
+           // string = Integer.toString(i);
+            //if (checkpalindrom(string) == true) {
+              //  System.out.println(string);
+            //}
+        //}
 
-    //if()
-
-
+    //}
 
 
+
+
+
+ // private static boolean checkpalindrom(String text) {
+   //     if (text.charAt(0) == text.charAt(5)) {
+     //   } else {
+         //   return false;
+       // }
+
+        //if (text.charAt(1) == text.charAt(4)) {
+       // } else {
+         //   return false;
+       // }
+
+        //if (text.charAt(2) == text.charAt(3)) {
+        //} else {
+         //   return false;
+       // }
+
+        //return true;
     }
+ }
 
-    private static boolean checkpalindrom(String text) {
-        if (text.charAt(0) == text.charAt(3)) {
-        } else {
 
-            return false;
-        }
 
-        if (text.charAt(1) == text.charAt(2)) {
-        } else {
-
-            return false;
-        }
-          return true;
-    }
-
-}
 
